bab61a394e956cf18699575d6a903fb9ff1942e5
Sempitern <marcoswilian187@gmail.com>
2023-10-02 19:37:45 -0300

    Primeiro desafio Dio: Abrindo Contas.

---
+++ DesafiosPOO/ContaBancaria/src/Main.java
@@ -0,0 +1,45 @@
+import java.text.DecimalFormat;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        DecimalFormat df = new DecimalFormat("#.00");
+        Locale.setDefault(Locale.US);
+        Scanner scan = new Scanner (System.in);
+
+                int numeroConta = scan.nextInt();
+                scan.nextLine(); // Consome a quebra de linha após a entrada do número da conta
+                String nomeTitular = scan.nextLine();
+                double saldo = scan.nextDouble();
+
+                //TODO: Criar uma instância de "ContaBancaria" com os valores de Entrada.
+                ContaBancaria novoUsuario = new ContaBancaria(numeroConta, nomeTitular, saldo, df);
+                System.out.println("Informacoes:");
+
+                //TODO: Imprimir as informações da conta usando o objeto criado no TODO acima.
+                System.out.println(novoUsuario);
+    }
+}
+
+        class ContaBancaria {
+            int numero;
+            String titular;
+            double saldo;
+
+            DecimalFormat df;
+
+
+            public ContaBancaria(int numeroConta, String nomeTitular, double saldo, DecimalFormat df) {
+                this.numero = numeroConta;
+                this.titular = nomeTitular;
+                this.saldo = saldo;
+                this.df = df;
+            }
+
+            @Override
+            public String toString() {
+                return "Conta: " + numero + "\nTitular: " + titular + "\nSaldo: $" + df.format(saldo);
+            }
+        }
+
